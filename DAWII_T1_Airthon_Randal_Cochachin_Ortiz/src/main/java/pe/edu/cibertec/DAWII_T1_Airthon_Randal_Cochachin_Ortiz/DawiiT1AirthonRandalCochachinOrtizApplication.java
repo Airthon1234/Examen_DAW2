e10d4baded063081b1e34aa35147b63265a0c612
Airthon1234 <cipoteland@gmail.com>
2024-06-07 19:41:27 -0500

    Pregunta 1 primer commit

---
+++ DAWII_T1_Airthon_Randal_Cochachin_Ortiz/src/main/java/pe/edu/cibertec/DAWII_T1_Airthon_Randal_Cochachin_Ortiz/DawiiT1AirthonRandalCochachinOrtizApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_T1_Airthon_Randal_Cochachin_Ortiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiT1AirthonRandalCochachinOrtizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiT1AirthonRandalCochachinOrtizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWII_T1_Airthon_Randal_Cochachin_Ortiz/DawiiT1AirthonRandalCochachinOrtizApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_T1_Airthon_Randal_Cochachin_Ortiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiT1AirthonRandalCochachinOrtizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
